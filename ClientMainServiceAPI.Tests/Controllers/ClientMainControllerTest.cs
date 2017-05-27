02e0ab9cacd4abd9a5873e4629552481d7e411a9
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-05-27 11:36:21 -0300

    Commit inicial do projeto

---
+++ ClientMainServiceAPI.Tests/Controllers/ClientMainControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClientMainServiceAPI.Controllers;
+
+namespace ClientMainServiceAPI.Tests.Controllers
+{
+    [TestClass]
+    public class ClientMainControllerTest
+    {
+        [TestMethod]
+        public void GetByID()
+        {
+            // Arrange
+            ClientMainController controller = new ClientMainController();
+
+            // Act
+            ViewResult result = controller.GetByID("1") as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            //Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
---
+++ ClientMainServiceAPI/Controllers/ClientMainController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Http;
+
+namespace ClientMainServiceAPI.Controllers
+{
+    [RoutePrefix("api/ClientMain")]
+    public class ClientMainController : ApiController
+    {
+        [AcceptVerbs("GET")]
+        [Route("GetByID/{id}")]
+        public IHttpActionResult GetByID(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
